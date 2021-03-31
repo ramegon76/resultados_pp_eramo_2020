fb520f8df7329be1df194356918c4817dda7181a
Raul Mejía <ramegon76@gmail.com>
2021-03-31 07:35:42 -0600

    310321

--- pruebas.R
+++ pruebas.R
@@ -294,3 +294,215 @@ filePath <- "archivos/prods_gan.txt"
 text3 <- readLines(filePath,encoding="UTF-8") 
 frec3 <- sort(table(text3), decreasing = T)
   
+
+
+
+
+
+
+
+### Paquetes necesarios:
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+# Se lee el archivo txt:
+filePath <- "archivos/prods_estab.txt"
+text <- readLines(filePath,encoding="UTF-8")     #Leemos el archivo
+text = iconv(text, to="ASCII//TRANSLIT") # Convert Character Vector between Encodings
+corpus <- Corpus(VectorSource(text)) # formato de texto
+# llevamos a minúsculas
+d  <- tm_map(corpus, tolower)
+# quitamos espacios en blanco
+d  <- tm_map(d, stripWhitespace)
+# quitamos la puntuación
+d <- tm_map(d, removePunctuation)
+# quitamos los números
+d <- tm_map(d, removeNumbers)
+
+#Un comando útil es quitar la palabras genéricas, la paquetería “tm” tiene un grupo de palabras. Para verlas:
+# stopwords("spanish")
+# Importante: “stopwords(”spanish“)” es un vector, se puede ampliar y actualizar de forma personalizada.
+
+# remueve palabras vacías genericas
+d <- tm_map(d, removeWords, stopwords("spanish"))
+
+
+##### Conteo: 
+
+# Una vez “limpio” el texto, tenemos que pasarlo a un formato de base de datos.
+tdm <- TermDocumentMatrix(d)
+
+# Encontramos sus asociaciones;
+frecuentes<-findFreqTerms(tdm, lowfreq=20) # el parámetro lowfreq fija el umbral de frecuencia deseado
+# findAssocs(tdm, frecuentes, 0.45)  # se determinan las asociaciones
+
+
+##### Sumarización:
+
+m <- as.matrix(tdm)                            # lo vuelve una matriz
+v <- sort(rowSums(m),decreasing=TRUE)          # lo ordena y suma
+df <- data.frame(word = names(v),freq=v)       # lo nombra y hace data.frame
+
+# Y se hace la nube de palabras: 
+
+wordcloud(words = df$word, freq = df$freq, min.freq = 2,
+          max.words=100, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+
+
+
+
+
+
+
+prod_estab <- read.csv("archivos/productos_estab.csv")
+
+prods_princ <- which((prod_estab$Productos_genericos == "Quesos" | 
+                        prod_estab$tipo_prod == "Pollo" |
+                        prod_estab$tipo_prod == "Comida preparada" | 
+                        prod_estab$tipo_prod == "Carnes frías y embutidos" |
+                        prod_estab$tipo_prod == "Carne de res" | 
+                        prod_estab$tipo_prod == "Jitomate" |
+                        prod_estab$tipo_prod == "Tomate" | 
+                        prod_estab$tipo_prod == "Cebolla" |
+                        prod_estab$tipo_prod == "Carne de cerdo" | 
+                        prod_estab$tipo_prod == "Papa" | 
+                        prod_estab$tipo_prod == "Tortillas" |
+                        prod_estab$tipo_prod == "Huevo" | 
+                        prod_estab$tipo_prod == "Limón" | 
+                        prod_estab$tipo_prod == "Lechuga" |
+                        prod_estab$tipo_prod == "Aguacate" | 
+                        prod_estab$tipo_prod == "Naranja" |
+                        prod_estab$tipo_prod == "Pescados y mariscos" |
+                        prod_estab$tipo_prod == "Verduras" |
+                        prod_estab$tipo_prod == "Zanahoria" |
+                        prod_estab$tipo_prod == "Plátano") &
+                       prod_estab$u_prom_men %in% c(1,2)) 
+
+
+
+Producto <- as.factor(prod_estab$Productos_genericos[prods_princ])
+cant <- prod_estab$prom_men[prods_princ]
+unidad <- prod_estab$u_prom_men[prods_princ]
+kg <- ifelse(unidad == 1, cant, cant*1000)
+
+datos1 <- data.frame(Producto,kg)
+
+
+library(plotly)
+
+fig1 <- plot_ly(data = datos1, x = ~Producto, y = ~kg, type = "box")
+fig1 %>% layout(title = "Cantidad de producto vendido al mes")
+
+
+
+
+
+
+
+## Porcentajes
+
+
+prods_princ <- which((prod_estab$Productos_genericos == "Quesos" | 
+                        prod_estab$Productos_generico == "Pollo" |
+                        prod_estab$Productos_generico == "Comida preparada" | 
+                        prod_estab$Productos_generico == "Carnes frías y embutidos" |
+                        prod_estab$Productos_generico == "Carne de res" | 
+                        prod_estab$Productos_generico == "Jitomate" |
+                        prod_estab$Productos_generico == "Tomate" | 
+                        prod_estab$Productos_generico == "Cebolla" |
+                        prod_estab$Productos_generico == "Carne de cerdo" | 
+                        prod_estab$Productos_generico == "Papa" | 
+                        prod_estab$Productos_generico == "Tortillas" |
+                        prod_estab$Productos_generico == "Huevo" | 
+                        prod_estab$Productos_generico == "Limón" | 
+                        prod_estab$Productos_generico == "Lechuga" |
+                        prod_estab$Productos_generico == "Aguacate" | 
+                        prod_estab$Productos_generico == "Naranja" |
+                        prod_estab$Productos_generico == "Pescados y mariscos" |
+                        prod_estab$Productos_generico == "Verduras" |
+                        prod_estab$Productos_generico == "Zanahoria" |
+                        prod_estab$Productos_generico == "Plátano") &
+                       prod_estab$u_perdida_prod %in% c(1,2) &
+                       prod_estab$u_prom_men %in% c(1,2)) 
+
+Producto <- as.factor(prod_estab$Productos_genericos[prods_princ])
+cant1 <- prod_estab$perdida_prod[prods_princ]
+cant2 <- prod_estab$prom_men[prods_princ]
+unidad1 <- prod_estab$u_perdida_prod[prods_princ]
+unidad2 <- prod_estab$u_prom_men[prods_princ]
+kg1 <- ifelse(unidad1 == 1, cant1, cant1*1000)
+kg2 <- ifelse(unidad2 == 1, cant2, cant2*1000)
+porcent_kg <- kg1/kg2*100
+
+datos1 <- data.frame(Producto,porcent_kg)
+
+
+library(plotly)
+
+fig1 <- plot_ly(data = datos1, x = ~Producto, y = ~porcent_kg, type = "box")
+fig1 %>% layout(title = "Cantidad de producto perdido al mes (kg)")
+
+
+
+sort(table(prod_estab$Productos_genericos), decreasing = T)[1:25]
+as.numeric(prod_estab$din_perd[prods_princ])
+
+
+sum(as.numeric(prod_estab$din_perd[prod_estab$din_perd > 0]))
+
+
+
+
+
+
+prods_princ <- which((prod_estab$Productos_genericos == "Quesos" | 
+                        prod_estab$Productos_generico == "Pollo" |
+                        prod_estab$Productos_generico == "Comida preparada" | 
+                        prod_estab$Productos_generico == "Carnes frías y embutidos" |
+                        prod_estab$Productos_generico == "Carne de res" | 
+                        prod_estab$Productos_generico == "Jitomate" |
+                        prod_estab$Productos_generico == "Tomate" | 
+                        prod_estab$Productos_generico == "Cebolla" |
+                        prod_estab$Productos_generico == "Carne de cerdo" | 
+                        prod_estab$Productos_generico == "Papa" | 
+                        prod_estab$Productos_generico == "Tortillas" |
+                        prod_estab$Productos_generico == "Huevo" | 
+                        prod_estab$Productos_generico == "Limón" | 
+                        prod_estab$Productos_generico == "Lechuga" |
+                        prod_estab$Productos_generico == "Aguacate" | 
+                        prod_estab$Productos_generico == "Naranja" |
+                        prod_estab$Productos_generico == "Pescados y mariscos") &
+                       prod_estab$u_comestible %in% c(1,2) &
+                       prod_estab$u_perdida_prod %in% c(1,2)) 
+
+Producto <- as.factor(prod_estab$Productos_genericos[prods_princ])
+cant1 <- as.numeric(prod_estab$comestible[prods_princ])
+cant2 <- prod_estab$perdida_prod[prods_princ]
+unidad1 <- prod_estab$u_comestible[prods_princ]
+unidad2 <- prod_estab$u_perdida_prod[prods_princ]
+kg1 <- ifelse(unidad1 == 1, cant1, cant1*1000)
+kg2 <- ifelse(unidad2 == 1, cant2, cant2*1000)
+Porcentaje <- kg1/kg2*100
+
+
+library(plotly)
+
+fig1 <- plot_ly(data = datos1, x = ~Producto, y = ~Porcentaje, type = "box")
+fig1 %>% layout(title = "Prcentaje de producto perdido que aún era comestible")
+
+
+
+
+
+
+
+
+
+
+
+
